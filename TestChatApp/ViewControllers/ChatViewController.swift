3c5ff79ac45a919617292c75aaa441f334c19e6d
tesddev <tesleem.amuda@gmail.com>
2023-05-17 22:15:48 +0100

    add base view controllers

---
+++ TestChatApp/ViewControllers/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  TestChatApp
+//
+//  Created by Tes on 17/05/2023.
+//
+
+import UIKit
+
+class ChatViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ TestChatApp/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  TestChatApp
+//
+//  Created by Tes on 17/05/2023.
+//
+
+import UIKit
+
+class SettingsViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
