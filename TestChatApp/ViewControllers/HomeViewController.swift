f7e9ac033d781edccfded30456337ff330143793
tesddev <tesleem.amuda@gmail.com>
2023-05-19 23:45:18 +0100

    add views and subviews

--- TestChatApp/ViewControllers/HomeViewController.swift
+++ TestChatApp/ViewControllers/HomeViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class HomeViewController: BaseViewController {
-
+    var headerLabel: AppLabel = {
+        let label = AppLabel()
+        label.text = "Feed"
+        label.textColor = UIColor(hexString: "#55A99D")
+        return label
+    }()
+    
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hexString: "#F2F6F8")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        activateConstraint()
     }
-
+    
+    private func activateConstraint() {
+        view.addSubview(headerLabel)
+        view.addSubview(dividerView)
+        
+        NSLayoutConstraint.activate([
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            headerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            dividerView.heightAnchor.constraint(equalToConstant: 2),
+            dividerView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 16),
+            dividerView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            dividerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    @objc func didTapLoginButton(){
+        let vc = BaseTabBarViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
